2f1f42e4b0e2f1a2f40aaaf0f7efccd7bd0fb310
gaabs <gaabs@cin.ufpe.br>
2018-11-26 09:29:34 -0300

    Update main

--- src/main/java/br/ufpe/cin/experiments/Main.java
+++ src/main/java/br/ufpe/cin/experiments/Main.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.concurrent.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -20,8 +21,18 @@ import static com.google.common.base.Predicates.not;
 
 public class Main {
     private static final String SAMPLE_BASE_PATH = "/home/gio/Downloads/sample/";
-    private static final String SAMPLE_REVISIONS_FILENAME = "all.revisions";
+    private static final String SAMPLE_REVISIONS_FILENAME = "test.revisions";
     private static final String LOGS_BASE_PATH = "logs/";
+
+    private static final Map<String, Integer> numberOfFilesWithAnyConflict = new ConcurrentHashMap<>();
+    private static final Map<String, Integer> numberOfAnyConflict = new ConcurrentHashMap<>();
+    private static final Map<String, Integer> numberOfFilesWithRenamingConflict = new ConcurrentHashMap<>();
+    private static final Map<String, Integer> numberOfRenamingConflicts = new ConcurrentHashMap<>();
+
+    private static List<String> filesWithSafeRenamingConflict = new ArrayList<>();
+    private static List<String> filesWithNoSafeRenamingConflict = new ArrayList<>();
+
+    private static JFSTMerge jsFSTMerge;
 
     public static void main(String[] args) {
         Long startTime = System.currentTimeMillis();
@@ -29,91 +40,17 @@ public class Main {
             BufferedReader reader = Files.newBufferedReader(Paths.get(SAMPLE_BASE_PATH + SAMPLE_REVISIONS_FILENAME));
             List<String> listRevisions = reader.lines().collect(Collectors.toList());
 
-            JFSTMerge jsFSTMerge = new JFSTMerge();
+            jsFSTMerge = new JFSTMerge();
             JFSTMerge.isGit = true;
-
-            Map<String, Integer> numberOfFilesWithAnyConflict = new HashMap<>();
-            Map<String, Integer> numberOfAnyConflict = new HashMap<>();
-            Map<String, Integer> numberOfFilesWithRenamingConflict = new HashMap<>();
-            Map<String, Integer> numberOfRenamingConflicts = new HashMap<>();
-            List<String> revisionsWithRenamingConflict = new ArrayList<>();
-
-            List<String> filesWithSafeRenamingConflict = new ArrayList<>();
-            List<String> filesWithNoSafeRenamingConflict = new ArrayList<>();
 
             int currentRevisionNumber = 0;
             for (String revision : listRevisions) {
                 currentRevisionNumber++;
-                System.out.printf("At %d out of %d revisions\n", currentRevisionNumber, listRevisions.size());
+                System.out.printf("------- At %d out of %d revisions -------\n", currentRevisionNumber, listRevisions.size());
                 System.out.println(revision);
 
-                JFSTMerge.renamingStrategy = RenamingStrategy.SAFE;
-                MergeScenario mergeScenario = jsFSTMerge.mergeRevisions(revision);
-                mergeScenario.getTuples().stream()
-                        .peek(tuple -> {
-                            numberOfAnyConflict.merge("unstructured", tuple.getContext().unstructuredNumberOfConflicts, Integer::sum);
-                            numberOfFilesWithAnyConflict.merge("unstructured", tuple.getContext().unstructuredNumberOfConflicts > 0 ? 1 : 0, Integer::sum);
-
-                            numberOfAnyConflict.merge("semistructured", tuple.getContext().semistructuredNumberOfConflicts, Integer::sum);
-                            numberOfFilesWithAnyConflict.merge("semistructured", tuple.getContext().semistructuredNumberOfConflicts > 0 ? 1 : 0, Integer::sum);
-                        })
-//                        .filter(tuple -> tuple.getBaseFile() != null)
-//                        .filter(tuple -> tuple.getContext().renamingConflicts > 0)
-//                        .filter(tuple -> tuple.getLeftFile() != null && tuple.getBaseFile() != null && tuple.getRightFile() != null)
-//                        .filter(tuple -> !RenamingUtils.getMethodsOrConstructors(tuple.getContext().nodesDeletedByLeft).isEmpty() ||
-//                                !RenamingUtils.getMethodsOrConstructors(tuple.getContext().nodesDeletedByRight).isEmpty())
-//                        .filter(tuple -> {
-//                            JFSTMerge.renamingStrategy = RenamingStrategy.MERGE_METHODS;
-//                            MergeContext mergeContext = jsFSTMerge.mergeFiles(tuple.getLeftFile(),
-//                                    tuple.getBaseFile(),
-//                                    tuple.getRightFile(),
-//                                    null);
-//
-//                            return !mergeContext.semistructuredOutput.equals(tuple.getContext().semistructuredOutput);
-//                        })
-                        .forEach(tuple -> {
-                            String logPath = getLogPath(revision, tuple);
-
-                            if (tuple.getContext().renamingConflicts > 0) {
-                                filesWithNoSafeRenamingConflict.add(logPath);
-                            } else {
-                                filesWithSafeRenamingConflict.add(logPath);
-                                return;
-                            }
-
-                            System.out.println("Found renaming");
-                            System.out.println(mergeScenario.getRevisionsFilePath());
-                            System.out.println(tuple.getBaseFile().getAbsolutePath());
-                            System.out.println();
-                            revisionsWithRenamingConflict.add(revision);
-
-                            copyFile(tuple.getLeftFile(), logPath + "/left");
-                            copyFile(tuple.getBaseFile(), logPath + "/base");
-                            copyFile(tuple.getRightFile(), logPath + "/right");
-                            writeFile(logPath + "/UNSTRUCTURED", tuple.getContext().unstructuredOutput);
-
-                            for (RenamingStrategy strategy : RenamingStrategy.values()) {
-                                JFSTMerge.renamingStrategy = strategy;
-                                JFSTMerge.logFiles = true;
-                                MergeContext mergeContext = jsFSTMerge.mergeFiles(tuple.getLeftFile(),
-                                        tuple.getBaseFile(),
-                                        tuple.getRightFile(),
-                                        null);
-
-                                String outputPath = logPath + "/" + strategy;
-
-                                jsFSTMerge.mergeFiles(tuple.getLeftFile(),
-                                        tuple.getBaseFile(),
-                                        tuple.getRightFile(),
-                                        outputPath);
-
-                                System.out.println(strategy);
-                                System.out.println("semistructuredNumberOfConflicts: " + mergeContext.semistructuredNumberOfConflicts);
-                                System.out.println("renamingConflicts: " + mergeContext.renamingConflicts);
-                                numberOfRenamingConflicts.merge(strategy.name(), mergeContext.renamingConflicts, Integer::sum);
-                                numberOfFilesWithRenamingConflict.merge(strategy.name(), mergeContext.renamingConflicts > 0 ? 1 : 0, Integer::sum);
-                            }
-                        });
+                JFSTMerge.renamingStrategy = RenamingStrategy.MERGE_METHODS;
+                mergeRevision(revision);
             }
 
             writeListFile(LOGS_BASE_PATH + "filesWithSafeRenamingConflict.txt", filesWithSafeRenamingConflict);
@@ -132,7 +69,78 @@ public class Main {
         System.out.printf("took %.3f minutes\n", (endTime - startTime) / (1000.0 * 60));
     }
 
-    private static String getLogPath(String revision, FilesTuple tuple) {
+    private static void mergeRevision(String revision) {
+        MergeScenario mergeScenario = jsFSTMerge.mergeRevisions(revision);
+        mergeScenario.getTuples().stream()
+                .peek(tuple -> {
+                    numberOfAnyConflict.merge("unstructured", tuple.getContext().unstructuredNumberOfConflicts, Integer::sum);
+                    numberOfFilesWithAnyConflict.merge("unstructured", tuple.getContext().unstructuredNumberOfConflicts > 0 ? 1 : 0, Integer::sum);
+                    numberOfAnyConflict.merge("semistructured", tuple.getContext().semistructuredNumberOfConflicts, Integer::sum);
+                    numberOfFilesWithAnyConflict.merge("semistructured", tuple.getContext().semistructuredNumberOfConflicts > 0 ? 1 : 0, Integer::sum);
+                })
+//                        .filter(tuple -> tuple.getBaseFile() != null)
+//                        .filter(tuple -> tuple.getContext().renamingConflicts > 0)
+//                        .filter(tuple -> tuple.getLeftFile() != null && tuple.getBaseFile() != null && tuple.getRightFile() != null)
+//                        .filter(tuple -> !RenamingUtils.getMethodsOrConstructors(tuple.getContext().nodesDeletedByLeft).isEmpty() ||
+//                                !RenamingUtils.getMethodsOrConstructors(tuple.getContext().nodesDeletedByRight).isEmpty())
+//                        .filter(tuple -> {
+//                            JFSTMerge.renamingStrategy = RenamingStrategy.MERGE_METHODS;
+//                            MergeContext mergeContext = jsFSTMerge.mergeFiles(tuple.getLeftFile(),
+//                                    tuple.getBaseFile(),
+//                                    tuple.getRightFile(),
+//                                    null);
+//
+//                            return !mergeContext.semistructuredOutput.equals(tuple.getContext().semistructuredOutput);
+//                        })
+                .forEach(tuple -> {
+                    String logPath = getLogPath(revision, tuple, tuple.getContext().renamingConflicts > 0);
+
+                    if (tuple.getContext().renamingConflicts > 0) {
+                        filesWithSafeRenamingConflict.add(logPath);
+                    } else {
+                        filesWithNoSafeRenamingConflict.add(logPath);
+//                        return;
+                    }
+
+//                            System.out.println("Found renaming");
+//                            System.out.println(mergeScenario.getRevisionsFilePath());
+//                            System.out.println(logPath);
+//                            System.out.println();
+
+                    copyFile(tuple.getLeftFile(), logPath + "/left");
+                    copyFile(tuple.getBaseFile(), logPath + "/base");
+                    copyFile(tuple.getRightFile(), logPath + "/right");
+                    writeFile(logPath + "/UNSTRUCTURED", tuple.getContext().unstructuredOutput);
+
+                    for (RenamingStrategy strategy : RenamingStrategy.values()) {
+                        JFSTMerge.renamingStrategy = strategy;
+                        JFSTMerge.logFiles = true;
+                        MergeContext mergeContext = jsFSTMerge.mergeFiles(tuple.getLeftFile(),
+                                tuple.getBaseFile(),
+                                tuple.getRightFile(),
+                                null);
+
+                        String outputPath = logPath + "/" + strategy;
+
+                        jsFSTMerge.mergeFiles(tuple.getLeftFile(),
+                                tuple.getBaseFile(),
+                                tuple.getRightFile(),
+                                outputPath);
+
+//                                System.out.println(strategy);
+//                                System.out.println("semistructuredNumberOfConflicts: " + mergeContext.semistructuredNumberOfConflicts);
+//                                System.out.println("renamingConflicts: " + mergeContext.renamingConflicts);
+                        synchronized (numberOfRenamingConflicts) {
+                            numberOfRenamingConflicts.merge(strategy.name(), mergeContext.renamingConflicts, Integer::sum);
+                        }
+                        synchronized (numberOfFilesWithRenamingConflict) {
+                            numberOfFilesWithRenamingConflict.merge(strategy.name(), mergeContext.renamingConflicts > 0 ? 1 : 0, Integer::sum);
+                        }
+                    }
+                });
+    }
+
+    private static String getLogPath(String revision, FilesTuple tuple, boolean hasSafeRenamingConflict) {
         String revisionPath = revision.substring(0, revision.lastIndexOf("/")).replace("/home/gio/Downloads/sample/", "") + "/";
         revisionPath = revisionPath.replaceAll("/", ".");
 
@@ -145,7 +153,9 @@ public class Main {
         String fileRelativePath = fileAbsolutePath.substring(fileAbsolutePath.lastIndexOf("/") + 1)
                 .replaceAll("/", ".");
 
-        return LOGS_BASE_PATH + revisionPath + fileRelativePath;
+        return LOGS_BASE_PATH
+//                + (hasSafeRenamingConflict ? "conflict/" : "no-conflict/")
+                + revisionPath + fileRelativePath;
     }
 
     private static void copyFile(File sourceFile, String destinationPath) {
@@ -169,7 +179,7 @@ public class Main {
         }
     }
 
-    private static void writeListFile(String destination, List<String> list) {
+    private static void writeListFile(String destination, Iterable<String> list) {
         String lines = String.join("\n", list);
         writeFile(destination, lines);
     }
